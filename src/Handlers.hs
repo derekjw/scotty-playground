277e907c6fd86f15d4b13c8b92b9acf6e75fd729
Derek Williams <derek@fyrie.net>
2015-02-23 03:42:12 -0800

    simplify type of selectAllPeople

--- src/Handlers.hs
+++ src/Handlers.hs
@@ -6,25 +6,21 @@
 
 module Handlers where
 
-import           Control.Monad.Reader.Class   (MonadReader)
-import           Control.Monad.Trans.Resource (MonadResource)
-import           Data.Conduit                 (Source)
-import           Data.Conduit                 (Conduit, Sink, ($$), ($=))
-import qualified Data.Conduit.List            as CL
-import           Data.Monoid                  (mconcat)
-import           Data.Monoid                  (Monoid (mappend, mempty))
-import           Data.Text.Lazy               (Text)
-import           Database.Persist             (HasPersistBackend,
-                                               PersistEntityBackend, get,
-                                               selectSource)
-import           Database.Persist.Sql         (Entity, SqlPersistM, toSqlKey)
+import           Data.Conduit         (Source)
+import           Data.Conduit         (Conduit, Sink, ($$), ($=))
+import qualified Data.Conduit.List    as CL
+import           Data.Monoid          (mconcat)
+import           Data.Monoid          (Monoid (mappend, mempty))
+import           Data.Text.Lazy       (Text)
+import           Database.Persist     (get, selectSource)
+import           Database.Persist.Sql (Entity, SqlPersistM, toSqlKey)
 import           Model
 import           Text.Blaze.Html5
 
 getPeople :: SqlPersistM Html
 getPeople = selectAllPeople $= peopleEntityToHtml $$ foldSink
 
-selectAllPeople :: (MonadResource m, PersistEntityBackend Person ~ backend, MonadReader env m, HasPersistBackend env backend) => Source m (Entity Person)
+selectAllPeople :: Source SqlPersistM (Entity Person)
 selectAllPeople = selectSource [] []
 
 peopleEntityToHtml :: (Monad m) => Conduit (Entity Person) m Html
